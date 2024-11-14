926fae1cc6466582ee0be413d0e7ef0a8739537f
Liam Doocey <ljdoocey@gmail.com>
2024-11-14 13:27:37 +0000

    Week 5 Exercise 2.1 - DriverList

--- app/src/main/java/com/setu/f1geek/MainActivity.kt
+++ app/src/main/java/com/setu/f1geek/MainActivity.kt
@@ -4,44 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.setu.f1geek.ui.theme.F1GeekTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.TextField
+import com.setu.f1geek.model.Driver
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             F1GeekTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DriverList()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DriverList(modifier: Modifier = Modifier) {
+
+    val drivers = listOf(
+            Driver("Roland", "Ratzenberger", "RAT", 32),
+            Driver("Jochen", "Rindt", "RIN", 22),
+            Driver("Jules", "Bianchi", "BIA", 17),
     )
+
+    Column(modifier = modifier) {
+        TextField(value = "", onValueChange = {}, label = { Text("Search") })
+        drivers.forEach { driver ->
+            Text(text = "${driver.firstName} ${driver.surName} (${driver.number})")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     F1GeekTheme {
-        Greeting("Android")
+        DriverList()
     }
 }
