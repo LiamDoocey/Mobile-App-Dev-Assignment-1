f3c0c2b565931891d8a619976883ff8bf98f272c
Liam Doocey <ljdoocey@gmail.com>
2024-11-14 12:51:08 +0000

    Added Driver and Team classes

---
+++ app/src/main/java/com/setu/f1geek/model/Driver.kt
@@ -0,0 +1,8 @@
+package com.setu.f1geek.model
+
+class Driver(
+    val firstName: String,
+    val surName: String,
+    val abbreviatedName: String,
+    val number: Int
+)
---
+++ app/src/main/java/com/setu/f1geek/model/Team.kt
@@ -0,0 +1,8 @@
+package com.setu.f1geek.model
+
+class Team (
+    val name: String,
+    val primaryDriver: Driver,
+    val secondaryDriver: Driver,
+    val reserveDrivers: List<Driver>
+)
