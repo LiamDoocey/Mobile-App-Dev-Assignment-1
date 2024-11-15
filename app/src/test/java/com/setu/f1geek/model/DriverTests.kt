aba0a4c88562e15b8afc859ccebeda3e90fd9ca8
Liam Doocey <ljdoocey@gmail.com>
2024-11-15 16:40:18 +0000

    Updated tests.

--- app/src/test/java/com/setu/f1geek/model/DriverTests.kt
+++ app/src/test/java/com/setu/f1geek/model/DriverTests.kt
@@ -17,25 +17,31 @@ class DriverTest {
 
     @Test
     fun getFirstName() {
-        val driver = Driver("Lewis", "Hamilton", "HAM", 44)
+        val driver = Driver("Lewis", "Hamilton", "HAM", 44, 39)
         assertEquals("Lewis", driver.firstName)
     }
 
     @Test
     fun getSurName() {
-        val driver = Driver("Lewis", "Hamilton", "HAM", 44)
+        val driver = Driver("Lewis", "Hamilton", "HAM", 44, 39)
         assertEquals("Hamilton", driver.surName)
     }
 
     @Test
     fun getAbbreviatedName() {
-        val driver = Driver("Lewis", "Hamilton", "HAM", 44)
+        val driver = Driver("Lewis", "Hamilton", "HAM", 44, 39)
         assertEquals("HAM", driver.abbreviatedName)
     }
 
     @Test
     fun getNumber() {
-        val driver = Driver("Lewis", "Hamilton", "HAM", 44)
+        val driver = Driver("Lewis", "Hamilton", "HAM", 44, 39)
         assertEquals(44, driver.number)
+    }
+
+    @Test
+    fun getAge() {
+        val driver = Driver("Lewis", "Hamilton", "HAM", 44, 39)
+        assertEquals(39, driver.age)
     }
 }
--- app/src/test/java/com/setu/f1geek/model/TeamTests.kt
+++ app/src/test/java/com/setu/f1geek/model/TeamTests.kt
@@ -9,46 +9,46 @@ class TeamTests {
 
     @Test
     fun getName() {
-        val charles = Driver("Charles", "Leclerc", "CL", 16)
-        val carlos = Driver("Carlos", "Sainz", "CS", 55)
-        val team = Team("Ferrari", charles, carlos, mutableListOf())
+        val charles = Driver("Charles", "Leclerc", "CL", 16, 23)
+        val carlos = Driver("Carlos", "Sainz", "CS", 55, 26)
+        val team = Team("Ferrari", charles, carlos, mutableListOf(), 0)
 
         assertEquals("Ferrari", team.name)
     }
 
     @Test
     fun getPrimaryDriver() {
-        val charles = Driver("Charles", "Leclerc", "CL", 16)
-        val carlos = Driver("Carlos", "Sainz", "CS", 55)
-        val team = Team("Ferrari", charles, carlos, mutableListOf())
+        val charles = Driver("Charles", "Leclerc", "CL", 16, 23)
+        val carlos = Driver("Carlos", "Sainz", "CS", 55, 26)
+        val team = Team("Ferrari", charles, carlos, mutableListOf(), 0)
 
         assertEquals(charles, team.primaryDriver)
     }
 
     @Test
     fun getSecondaryDriver() {
-        val charles = Driver("Charles", "Leclerc", "CL", 16)
-        val carlos = Driver("Carlos", "Sainz", "CS", 55)
-        val team = Team("Ferrari", charles, carlos, mutableListOf())
+        val charles = Driver("Charles", "Leclerc", "CL", 16, 23)
+        val carlos = Driver("Carlos", "Sainz", "CS", 55, 26)
+        val team = Team("Ferrari", charles, carlos, mutableListOf(), 0)
 
         assertEquals(carlos, team.secondaryDriver)
     }
 
     @Test
     fun getReserveDrivers() {
-        val charles = Driver("Charles", "Leclerc", "CL", 16)
-        val carlos = Driver("Carlos", "Sainz", "CS", 55)
-        val team = Team("Ferrari", charles, carlos, mutableListOf())
+        val charles = Driver("Charles", "Leclerc", "CL", 16, 23)
+        val carlos = Driver("Carlos", "Sainz", "CS", 55, 26)
+        val team = Team("Ferrari", charles, carlos, mutableListOf(), 0)
 
         assertEquals(mutableListOf<Driver>(), team.reserveDrivers)
     }
 
     @Test
     fun setPrimaryAndSecondaryDriverToSameDriver(){
-        val charles = Driver("Charles", "Leclerc", "CL", 16)
+        val charles = Driver("Charles", "Leclerc", "CL", 16, 23)
 
         assertThrows<IllegalStateException> {
-            Team("Ferrari", charles, charles, mutableListOf())
+            Team("Ferrari", charles, charles, mutableListOf(), 0)
         }
     }
 }
